1f2b6a3e6291bde3f43c236ac1cbbf734a12007e
Delta-269 <kondosoma12@gmail.com>
2020-10-27 01:08:31 +0900

    Userモデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nick_name, presence: true
-  validates :name_zen_family, presence: true
-  validates :name_zen_first, presence: true
-  validates :name_han_family, presence: true
-  validates :name_han_first, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-ん一-龥]+\z/ } do
+    validates :name_zen_family, presence: true
+    validates :name_zen_first, presence: true 
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+    validates :name_han_family, presence: true
+    validates :name_han_first, presence: true
+  end
+
   validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
 end
