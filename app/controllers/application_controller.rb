1b4b09b9b6de7f061c0bfa31386d3e1bf8297986
Delta-269 <kondosoma12@gmail.com>
2020-10-27 00:52:33 +0900

    新規登録機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :name_zen_family, :name_zen_first, :name_han_family, :name_han_first, :birthday])
+  end
+
 end
