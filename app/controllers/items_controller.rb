825b9f02234437fd21efb66def14148b40d5d75c
Delta-269 <kondosoma12@gmail.com>
2020-10-27 18:50:51 +0900

    商品出品ページ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,26 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! [:new]
+  before_action :authenticate_user!, only: [:new]
 
   def index
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+    
 
   private
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :burden_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
