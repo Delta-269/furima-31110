5579be6f959a33f4fa040bd14d4e6bffae1149e0
Delta-269 <kondosoma12@gmail.com>
2020-10-28 16:26:38 +0900

    商品出品機能単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,18 +12,22 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :category
-    validates :status
-    validates :burden
-    validates :area
-    validates :day
-    validates :price
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :day_id
+    validates :price, format: { with: /\A[0-9]+\z/ },
+    numericality:{greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999 } 
     validates :image
   end
 
-  validates :category_id, numericality: { other_than: 1 }
-  validates :status_id, numericality: { other_than: 1 }
-  validates :burden_id, numericality: { other_than: 1 }
-  validates :area_id, numericality: { other_than: 1 }
-  validates :day_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :day_id
+  end
 end
